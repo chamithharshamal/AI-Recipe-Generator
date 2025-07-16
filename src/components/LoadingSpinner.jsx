1ae3c98e08759aeca6ee138361fea49386787ff9
Chamith Harshamal <chamithweerasinghe1@gmail.com>
2025-07-16 11:36:01 +0530

    Reorganized repository with client and server folders in root

--- src/components/LoadingSpinner.jsx
+++
@@ -1,9 +0,0 @@
-function LoadingSpinner() {
-  return (
-    <div className="flex justify-center items-center">
-      <div className="w-8 h-8 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
-    </div>
-  );
-}
-
-export default LoadingSpinner;
--- src/pages/Recipe.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import RecipeForm from '../components/Recipe/RecipeForm';
-
-const RecipePage = () => {
-  return <RecipeForm />;
-};
-
-export default RecipePage;
